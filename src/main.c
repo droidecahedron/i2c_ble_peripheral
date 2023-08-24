ff6a46d18783de0537f020a09b371cccd88897b4
droidecahedron <droidecahedron@gmail.com>
2023-08-24 09:58:41 -0700

    sensor working, moving on to adding power management

--- src/main.c
+++ src/main.c
@@ -8,63 +8,33 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 
-#include <zephyr/drivers/i2c.h>
 #include <zephyr/logging/log.h>
 
+#include "inc/si7021.h"
 
-#define SLEEP_TIME_MS   1000
 
-#define TEST_DATA_HIGH_REG            0x00
-#define TEST_DATA_LOW_REG             0x02
-#define TEST_CONFIG_REG               0x03
+#define SLEEP_TIME_MS   5000
 
-#define I2C_NODE DT_NODELABEL(mysensor)
 
 LOG_MODULE_REGISTER(i2c_ble_p);
 
 void main(void)
 {
 
-	int ret;
+    int ret;
+    float humidity;
+    float temperature_C;
 
-	static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C_NODE);
-	if (!device_is_ready(dev_i2c.bus)) {
-		printk("I2C bus %s is not ready!\n\r",dev_i2c.bus->name);
-		return;
-	}
+    si7021_init();
+    k_msleep(100); // give it some time after resetting.
 
-	uint8_t config[2] = {TEST_CONFIG_REG,0x8C};
-	ret = i2c_write_dt(&dev_i2c, config, sizeof(config));
-	if(ret != 0){
-		printk("Failed to write to I2C device address %x at Reg. %x \n", dev_i2c.addr,config[0]);
-		return;
-	}
 
-	while (1) {
-		uint8_t temp_reading[2]= {0};
-		uint8_t sensor_regs[2] ={TEST_DATA_LOW_REG,TEST_DATA_HIGH_REG};
-		ret = i2c_write_read_dt(&dev_i2c,&sensor_regs[0],1,&temp_reading[0],1);
-		if(ret != 0){
-			printk("Failed to write/read I2C device address %x at Reg. %x \r\n", dev_i2c.addr,sensor_regs[0]);
-		}
-		ret = i2c_write_read_dt(&dev_i2c,&sensor_regs[1],1,&temp_reading[1],1);
-		if(ret != 0){
-			printk("Failed to write/read I2C device address %x at Reg. %x \r\n", dev_i2c.addr,sensor_regs[1]);
-		}
 
-		int temp = ((int)temp_reading[1] * 256 + ((int)temp_reading[0] & 0xF0)) / 16;
-		if(temp > 2047)
-		{
-			temp -= 4096;
-		}
+    while (1) {
+        humidity = si7021_read_humidity();
+        temperature_C = si7021_read_temperature();
 
-		// Convert to engineering units 
-		double cTemp = temp * 0.0625;
-		double fTemp = cTemp * 1.8 + 32;
-
-		//Print reading to console  
-		printk("Temperature in Celsius : %.2f C \n", cTemp);
-		printk("Temperature in Fahrenheit : %.2f F \n", fTemp);
-		k_msleep(SLEEP_TIME_MS);
-	}
+        LOG_INF("Humidity: %.2f, Temperature (C) : %.2f", humidity, temperature_C);
+        k_msleep(SLEEP_TIME_MS);
+    }
 }
---
+++ src/si7021.c
@@ -0,0 +1,108 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+
+#include <zephyr/drivers/i2c.h>
+
+#include "inc/si7021.h"
+
+LOG_MODULE_REGISTER(si7021);
+
+#define I2C_NODE DT_NODELABEL(si7021)
+
+bool si7021_init(void)
+{
+    int err;
+    static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C_NODE);
+    if (!device_is_ready(dev_i2c.bus))
+    {
+        printk("I2C bus %s is not ready!\n\r", dev_i2c.bus->name);
+        return false;
+    }
+
+    uint8_t cmd[] = {SI7021_RESET_CMD};
+    err = i2c_write_dt(&dev_i2c, cmd, sizeof(cmd));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write to i2c dev addr\n");
+        return false;
+    }
+
+    return true;
+}
+
+float si7021_read_humidity(void)
+{
+    int err;
+    float relative_humidity;
+
+    static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C_NODE);
+    if (!device_is_ready(dev_i2c.bus))
+    {
+        printk("I2C bus %s is not ready!\n\r", dev_i2c.bus->name);
+        return false;
+    }
+
+    // start conversion
+    uint8_t cmd[] = {SI7021_MEASRH_NOHOLD_CMD};
+    uint8_t buf[2] = {0};
+    err = i2c_write_dt(&dev_i2c, cmd, sizeof(cmd));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write/read to i2c dev\n");
+        return SI7021_ERRCODE;
+    }
+
+    k_msleep(20); // conversion time
+
+    err = i2c_read_dt(&dev_i2c, buf, sizeof(buf));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write/read to i2c dev\n");
+        return SI7021_ERRCODE;
+    }
+
+    relative_humidity = (125 * ((buf[0] << 8) | buf[1]) / 65536) - 6;
+    return relative_humidity;
+}
+
+float si7021_read_temperature(void)
+{
+    int err;
+    float temperature_C;
+
+    static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C_NODE);
+    if (!device_is_ready(dev_i2c.bus))
+    {
+        printk("I2C bus %s is not ready!\n\r", dev_i2c.bus->name);
+        return false;
+    }
+
+    // start conversion
+    uint8_t cmd[] = {SI7021_MEASTEMP_NOHOLD_CMD};
+    uint8_t buf[2] = {0};
+    err = i2c_write_dt(&dev_i2c, cmd, sizeof(cmd));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write/read to i2c dev\n");
+        return SI7021_ERRCODE;
+    }
+
+    k_msleep(20); // conversion time
+
+    err = i2c_read_dt(&dev_i2c, buf, sizeof(buf));
+    if (err != 0)
+    {
+        LOG_WRN("Failed to write/read to i2c dev\n");
+        return SI7021_ERRCODE;
+    }
+
+    //unit conversion
+    temperature_C = (175.72 * ((buf[0] << 8) | buf[1]) / 65536) - 46.85;
+    return temperature_C;
+}
